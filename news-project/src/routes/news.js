58acdf4910b5b010dde4c2aeabdbb98c948c9828
anuj <anuj@C40LAP-10090.india.rsystems.com>
2022-12-24 23:57:21 +0530

    news-website-03 - api dev

--- news-project/src/routes/news.js
+++ news-project/src/routes/news.js
@@ -1,9 +1,12 @@
 const express = require("express");
-
 const newsRouter = express.Router();
+const axios = require("axios");
 
 newsRouter.get("", async (req, res) => {
-  res.render("news");
+  // res.render("news");
+  try {
+    const newsAPI = await axios.get("");
+  } catch(error) {}
 });
 
 module.exports = newsRouter;
