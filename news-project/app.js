48eb71196235222e33bac8c202aa31a4b3f7c5ef
anuj <anuj@C40LAP-10090.india.rsystems.com>
2022-12-26 22:51:11 +0530

    news-website-03 - api

--- news-project/app.js
+++ news-project/app.js
@@ -1,17 +1,36 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = 5000;
 
+
+// Cross Origin Resource Sharing
+const whitelist = ["http://localhost:5000", "http://192.168.1.37:5000"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
 // static files
 app.use(express.static("public"));
-app.use("/css", express.static(__dirname + "public/css"));
-app.use("/img", express.static(__dirname + "public/img"));
-app.use("/js", express.static(__dirname + "public/js"));
+// app.use("/css", express.static(__dirname + "public/css"));
+// app.use("/img", express.static(__dirname + "public/img"));
+// app.use("/js", express.static(__dirname + "public/js"));
 
 // templating engine
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+
+// parsing middleware
+// app.use(express.urlencoded({extended: true}));
 
 // routes
 const newsRouter = require("./src/routes/news");
