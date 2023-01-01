56702896e6762899506bc1c39cb9039306cd0cd4
anuj <anuj@C40LAP-10090.india.rsystems.com>
2023-01-01 22:38:42 +0530

    node-mysql-crud-01 - eslint config.

---
+++ node-mysql-crud/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'linebreak-styles': 0,
+    'no-console': 0,
+  },
+};
