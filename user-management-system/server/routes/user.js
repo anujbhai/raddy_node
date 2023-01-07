cc4e4ddcd05e97e0ce7302e1537f4f66a6e7bd08
anuj <anuj@C40LAP-10090.india.rsystems.com>
2023-01-07 23:33:25 +0530

    user-mang-system - 03 - search.

--- user-management-system/server/routes/user.js
+++ user-management-system/server/routes/user.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require('../controllers/userControllers');
 
 router.get('/', userController.view);
+router.post('/', userController.find);
 
 module.exports = router;
